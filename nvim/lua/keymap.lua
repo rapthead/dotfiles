ffe1a18e26bb716af448d46647d6ccb4e7f06059
Павел Грибанов <rapthead@gmail.com>
2025-03-23 12:53:14 +0300

    improve neovim lsp support

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -18,48 +18,56 @@ vim.api.nvim_create_autocmd("InsertLeave", {
     end,
 })
 
+local function lspAttach(args)
+	-- Mappings.
+	local opts = { noremap=true, silent=true, buffer = bufnr }
+
+	local function inPrevWindowHandler(err, result, ctx, config)
+		vim.api.nvim_command('wincmd p')
+		vim.lsp.handlers[ctx.method](err, result, ctx, config)
+	end
+
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	vim.keymap.set('n', 'grn', vim.lsp.buf.rename, opts)
+	vim.keymap.set('n', 'grs', vim.lsp.buf.workspace_symbol, opts)
+	vim.keymap.set('n', 'grh', vim.lsp.buf.typehierarchy, opts)
+	vim.keymap.set('n', 'gra', vim.lsp.buf.code_action, opts)
+	vim.keymap.set('n', 'grc', vim.diagnostic.setqflist, opts)
+	vim.keymap.set('n', 'grI', vim.lsp.buf.incoming_calls, opts)
+	vim.keymap.set('n', 'grO', vim.lsp.buf.outgoing_calls, opts)
+	vim.keymap.set('n', 'grr', vim.lsp.buf.references, opts)
+	vim.keymap.set('n', 'gri', vim.lsp.buf.implementation, opts)
+	vim.keymap.set('n', 'gO', vim.lsp.buf.document_symbol, opts)
+	vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, opts)
+
+	vim.keymap.set('n', 'grD', vim.lsp.buf.declaration, opts)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+	vim.keymap.set('n', 'gv', function()
+		vim.lsp.buf_request(
+			0,
+			'textDocument/definition',
+			vim.lsp.util.make_position_params(),
+			inPrevWindowHandler
+		)
+	end, opts)
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+	vim.keymap.set('n', '<space>i', function()
+		vim.lsp.buf.code_action({
+			context = { only = { 'source.organizeImports' } },
+			apply = true,
+		})
+	end, opts)
+	vim.keymap.set('n', '<Leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+	vim.keymap.set('n', '<Leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+	vim.keymap.set('n', '<Leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+	vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
+	vim.keymap.set('n', '<C-k>', vim.diagnostic.goto_prev, opts)
+	vim.keymap.set('n', '<C-j>', vim.diagnostic.goto_next, opts)
+end
 vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(args)
-        -- Mappings.
-        local opts = { noremap=true, silent=true, buffer = bufnr }
-
-        local function inPrevWindowHandler(err, result, ctx, config)
-            vim.api.nvim_command('wincmd p')
-            vim.lsp.handlers[ctx.method](err, result, ctx, config)
-        end
-
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'gv', function()
-            vim.lsp.buf_request(
-                0,
-                'textDocument/definition',
-                vim.lsp.util.make_position_params(),
-                inPrevWindowHandler
-            )
-        end, opts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        vim.keymap.set('n', '<space>i', function()
-            vim.lsp.buf.code_action({
-                context = { only = { 'source.organizeImports' } },
-                apply = true,
-            })
-        end, opts)
-        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-		vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<C-k>', vim.diagnostic.goto_prev, opts)
-        vim.keymap.set('n', '<C-j>', vim.diagnostic.goto_next, opts)
-        -- vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, opts)
-    end,
+	callback = lspAttach,
 })
+lspAttach()
 
 -- vim.keymap.set({'i', 'n'}, 'jk', '<esc>', {noremap = true})
 -- vim.keymap.set({'i', 'n'}, '<esc>', '<nop>', {noremap = true})
